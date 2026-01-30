34678ce57878a4805af175bacee34edb830d576a
karnavgoyal <karnavgoyal2@gmailcom>
2026-01-30 21:27:51 +0530

    Names

---
+++ Array/NamesArray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class NamesArray{
+	public static void main(String[]args) {
+		
+		String[] names={"harshit","karnav","prince","saksham","Yug"};
+		 System.out.println("List of names:");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+	}
+}
